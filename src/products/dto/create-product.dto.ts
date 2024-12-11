d143e2e33cd1698dd5d3d3e3affa8647eecc2eb9
JosmenDev <josmendev@gmail.com>
2024-12-11 03:32:25 -0500

    CRUD con TypeORM y Postgres

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,39 @@
+import { ArrayNotEmpty, IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from 'class-validator';
+export class CreateProductDto {
+
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  // each is true quiere decir que cada elemento del arreglo tiene que ser un string
+  @IsString({ each: true})
+  @IsArray()
+  @ArrayNotEmpty()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+
+  @IsString({each: true})
+  @IsArray()
+  @IsOptional()
+  tags?: string[];
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,84 @@
+import { Injectable, InternalServerErrorException, BadRequestException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return await this.productRepository.find({
+      take: limit,
+      skip: offset
+    });
+  }
+
+  async findOne(term: string) {
+    let product: Product;
+    if( isUUID(term)) {
+      product = await this.productRepository.findOneBy({id: term});
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('title ILIKE :title or slug ILIKE :slug', {
+          title: term,
+          slug: term
+        }).getOne();
+    }
+    if(!product) throw new NotFoundException(`Product with term ${term} not found`);
+    return product;
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto
+    });
+    if(!product) throw new NotFoundException(`Product with id ${id} not found`);
+    try {
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    try {
+      await this.productRepository.remove(product);
+      return `He product with id ${id} is delete`;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  private handleDBExceptions (error: any) {
+    if(error.code === '23505') throw new BadRequestException(error.detail);
+    console.log(error);
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
+}
